4a9c7d8f3ba05276a1fc8c420317aaaaeb9f7e22
Alejandro Lledo <a.lledo@fp.safavalladolid.com>
2024-01-30 09:31:59 +0100

    ejercicio5.2 corregido todo menos la k

--- Tema5/5_2.sql
+++ Tema5/5_2.sql
@@ -1,3 +1,12 @@
+
+CREATE TABLE DEPARTAMENTO(
+    CLAVE_DEPTO NUMBER(1),
+    NOMBRE VARCHAR(30) NOT NULL,
+    PRESUPUESTO NUMBER(5),
+    
+    CONSTRAINT PK_DEPARTAMENTO PRIMARY KEY (CLAVE_DEPTO)
+);
+
 CREATE TABLE EMPLEADOS(
     CLAVE_EMPLEADO VARCHAR(5),
     NOMBRE VARCHAR(30) NOT NULL,
@@ -7,32 +16,25 @@ CREATE TABLE EMPLEADOS(
    CONSTRAINT PK_EMPLEADOS PRIMARY KEY (CLAVE_EMPLEADO),
    CONSTRAINT FK_DEPTO FOREIGN KEY (CLAVE_DEPTO) REFERENCES DEPARTAMENTO(CLAVE_DEPTO)ON DELETE CASCADE
 );
-CREATE TABLE DEPARTAMENTO(
-    CLAVE_DEPTO NUMBER(1),
-    NOMBRE VARCHAR(30) NOT NULL,
-    PRESUPUESTO NUMBER(5),
-    
-    CONSTRAINT PK_DEPARTAMENTO PRIMARY KEY (CLAVE_DEPTO)
-);
 
 CREATE SEQUENCE SEQ_DEPTO START WITH 1  INCREMENT BY 1 MAXVALUE 999 NOCYCLE;
 
 INSERT INTO departamento(clave_depto, nombre, presupuesto) VALUES (SEQ_DEPTO.NEXTVAL, 'PERSONAL', 80000);
-INSERT INTO departamento(clave_depto, nombre, presupuesto) VALUES (SEQ_DEPTO.NEXTVAL, 'ALMACÉN', 70000);
+INSERT INTO departamento(clave_depto, nombre, presupuesto) VALUES (SEQ_DEPTO.NEXTVAL, 'ALMACï¿½N', 70000);
 INSERT INTO departamento(clave_depto, nombre, presupuesto) VALUES (SEQ_DEPTO.NEXTVAL, 'CONTABILIDAD', 60000);
 INSERT INTO departamento(clave_depto, nombre, presupuesto) VALUES (SEQ_DEPTO.NEXTVAL, 'MANUFACTURA', 50000);
 INSERT INTO departamento(clave_depto, nombre, presupuesto) VALUES (SEQ_DEPTO.NEXTVAL, 'EMPAQUE', 30000);
 
-INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP01', 'Armando', 'López' , 2);
-INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP02', 'Tatiana', 'Várgas' , 1);
-INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP03', 'Laura', 'García' , 3);
-INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP04', 'Juan', 'Pérez' , 4);
-INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP05', 'Iván', 'López' , 5);
-INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP06', 'Margarita', 'Hernández' , 1);
-INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP07', 'Jesús', 'Pérez' , 3);
-INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP08', 'Toño', 'Flores' , 2);
-INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP09', 'Juan', 'López' , 4);
-INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP10', 'José', 'Hernández' , 5);
+INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP01', 'Armando', 'Lï¿½pez' , 2);
+INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP02', 'Tatiana', 'Vï¿½rgas' , 1);
+INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP03', 'Laura', 'Garcï¿½a' , 3);
+INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP04', 'Juan', 'Pï¿½rez' , 4);
+INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP05', 'Ivï¿½n', 'Lï¿½pez' , 5);
+INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP06', 'Margarita', 'Hernï¿½ndez' , 1);
+INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP07', 'Jesï¿½s', 'Pï¿½rez' , 3);
+INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP08', 'Toï¿½o', 'Flores' , 2);
+INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP09', 'Juan', 'Lï¿½pez' , 4);
+INSERT INTO EMPLEADOS (CLAVE_EMPLEADO, NOMBRE, APELLIDO1, CLAVE_DEPTO) VALUES ('EMP10', 'Josï¿½', 'Hernï¿½ndez' , 5);
 
 
 -- a)
@@ -40,25 +42,22 @@ SELECT nombre,apellido1 FROM empleados;
 -- b)
 SELECT DISTINCT apellido1 FROM empleados;
 -- c)
-SELECT * FROM empleados WHERE apellido1 = 'López';
+SELECT * FROM empleados WHERE apellido1 = 'Lï¿½pez';
 -- d)
-SELECT * FROM empleados WHERE nombre LIKE '%a';
+SELECT empleados.nombre, apellido1, departamento.nombre FROM empleados, departamento WHERE empleados.clave_depto = departamento.clave_depto AND empleados.nombre LIKE '%a';
 -- e)
-SELECT apellido1 FROM empleados WHERE apellido1 IN('López', 'Pérez');
+SELECT apellido1 FROM empleados, departamento WHERE apellido1 IN('Lï¿½pez', 'Pï¿½rez');
 -- f)
-SELECT empleados.nombre, clave_depto FROM empleados WHERE CLAVE_DEPTO = 4;
+SELECT empleados.nombre, empleados.clave_empleado FROM empleados WHERE CLAVE_DEPTO = 4;
 -- g)
 SELECT NOMBRE, APELLIDO1 FROM empleados WHERE apellido1 LIKE 'H%';
 -- h)
 SELECT * FROM departamento WHERE presupuesto BETWEEN 50000 AND 70000;
 -- i)
-SELECT clave_empleado, nombre, apellido1 FROM empleados WHERE clave_depto = 3;
+SELECT empleado.clave_empleado, empleado.nombre, empleado.apellido1 FROM empleados, departamento WHERE empleado.clave_depto = departamento.clave_depto AND departamento.nombre = 'contabilidad';
 -- j)
 SELECT empleados.nombre, departamento.nombre, empleados.apellido1 FROM empleados, departamento WHERE empleados.clave_depto = departamento.clave_depto;
 -- k)
 SELECT SUM(PRESUPUESTO)/10 FROM departamento;
 -- l)
 SELECT * FROM empleados WHERE nombre LIKE 'J%' AND clave_depto = 4;
-
-
-
